57c55accdb094c62c73a1c2786bf42da393969fb
ARBNonay32 <60768976+ARBNonay32@users.noreply.github.com>
2020-03-25 19:18:04 -0600

    added support for taking pictures
    barcode detector.builder at line 120 in the payment fragment does not work properly

--- Newber/app/src/main/java/com/cmput301w20t23/newber/views/RequestPaymentFragment.java
+++ Newber/app/src/main/java/com/cmput301w20t23/newber/views/RequestPaymentFragment.java
@@ -1,14 +1,18 @@
 package com.cmput301w20t23.newber.views;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +20,9 @@ import com.cmput301w20t23.newber.R;
 import com.cmput301w20t23.newber.controllers.RideController;
 import com.cmput301w20t23.newber.controllers.UserController;
 import com.cmput301w20t23.newber.models.RideRequest;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.zxing.WriterException;
 
 import androidmads.library.qrgenearator.QRGContents;
@@ -34,6 +41,8 @@ public class RequestPaymentFragment extends Fragment {
     private Bitmap bitmap;
     private QRGEncoder qrEncoder;
     private ImageView qrImage;
+    private ImageView qrPicture;
+    private BarcodeDetector detector;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
@@ -57,6 +66,7 @@ public class RequestPaymentFragment extends Fragment {
         create = (Button) view.findViewById(R.id.qr_create);
         scan = (Button) view.findViewById(R.id.qr_scan);
         qrImage = (ImageView)  view.findViewById(R.id.qr_image);
+        qrPicture = (ImageView) view.findViewById(R.id.qr_picture);
 
         if(role.equals("Rider")){
             scan.setVisibility(View.VISIBLE);
@@ -64,6 +74,8 @@ public class RequestPaymentFragment extends Fragment {
         else if(role.equals("Driver")) {
             create.setVisibility(View.INVISIBLE);
         }
+
+
 
         create.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -81,10 +93,34 @@ public class RequestPaymentFragment extends Fragment {
         scan.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-            dispatchTakePictureIntent();
+                dispatchTakePictureIntent();
+                //onActivityResult runs automatically after dispatchTakePictureIntent
+                BitmapDrawable drawable = (BitmapDrawable) qrPicture.getDrawable();
+                bitmap = drawable.getBitmap();
+                if(detector.isOperational() && bitmap != null)
+                {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<Barcode> barcodes = detector.detect(frame);
+                    if(barcodes.size() != 0)
+                    {
+                        Barcode code = barcodes.valueAt(0);
+                        TextView text = (TextView) view.findViewById(R.id.testqr);
+
+                    }
+                }
             }
         });
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if(getActivity() != null) {
+            detector = new BarcodeDetector.Builder(getActivity())
+                    .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                    .build();
+        }
     }
 
     @Override
@@ -92,10 +128,10 @@ public class RequestPaymentFragment extends Fragment {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            qrImage.setImageBitmap(imageBitmap);
+            qrPicture.setImageBitmap(imageBitmap);
         }
     }
-    
+
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
